8357c3badf733127b44a4d384327bca9d3fd4bea
dmigwi <migwindungu0@gmail.com>
2023-11-23 17:18:02 +0100

    Add the book table migrations

---
+++ database/migrations/2023_11_23_160841_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("title",1024);
+            $table->integer("year");
+            $table->string("publication_place");
+            $table->integer("pages");
+            $table->double("price");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
